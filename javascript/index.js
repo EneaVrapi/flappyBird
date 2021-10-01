7d1613b9a6a5947f514d96c703d4cf5c0fb0bf22
EneaVrapi <eneavrapi2006@gmail.com>
2021-10-01 17:10:40 +0200

    shtova funsion start dhe buton fillo

--- javascript/index.js
+++ javascript/index.js
@@ -1,8 +1,10 @@
 let canvas = document.querySelector('canvas')
 let ctx = canvas.getContext('2d')
 let intervalId = 0
+let muzik = new Audio()
+muzik.src="https://www.learningcontainer.com/wp-content/uploads/2020/02/Kalimba.mp3"
 //do kapim imazhet   
-
+let gameOver = document.querySelector('h2')
 //sfondi
 let sfondi = new Image()
 sfondi.src = 'https://wallpaperaccess.com/full/4622710.png'
@@ -26,17 +28,21 @@ tubiPoshte.src='https://raw.githubusercontent.com/sil-sin/flappy-bird-starter/ma
 
 //renja ,piket 
 
+let start = document.getElementById('start')
+
 let piket=0,renja=2
 
 document.addEventListener('mousedown',function(){
-    renja = 0
+    renja = -2
 })
 
 document.addEventListener('mouseup',function(){
-    renja = 0
+    renja = 1
 })
 
 let tubat = [{x:canvas.width-40,y:0}]
+
+let kutia = document.getElementById('kutia')
 
 function vizato(){
     ctx.drawImage(sfondi,0,0,300,500)
@@ -61,11 +67,16 @@ for (let i = 0 ; i < tubat.length ; i++){
     }
 
     if(zoguX + zogu.width > tubat[i].x && zoguX < tubat[i].x + tubiLarte.width && (zoguY < tubat[i].y + tubiLarte.height || zoguY + zogu.height > tubat[i].y + hapsire )){
-        alert('GAMEOVER')
+        
         clearInterval(intervalId)
         location.reload()
+        muzik.reload()
+        kutia.style.display = 'block'
+        canvas.style.display = 'none'
+        gameOver.innerText = `GAMEOVER ${piket} pike`
     }else{
         zoguY = zoguY + renja
+
     }
 }
 
@@ -73,9 +84,13 @@ for (let i = 0 ; i < tubat.length ; i++){
 ctx.drawImage(toka,0,canvas.height-80)
 
 if(zoguY > canvas.height -100){
-    alert('GAMEOVER')
+   
     clearInterval(intervalId)
     location.reload()
+    muzik.reload()
+    kutia.style.display = 'block'
+    canvas.style.display = 'none'
+    gameOver.innerText = `GAMEOVER ${piket} pike`
 }else{
     zoguY += renja
 }
@@ -84,30 +99,43 @@ ctx.font='20px Calibri'//madhesia dhe lloi i shkrimit font
 ctx.fillStyle='red'
 ctx.fillText('Pike: '+piket,20,canvas.height-30)
 
-console.log('U vizatua sfondi')
+
+}
+
+function fillo(){
+    canvas.style.display='block'
+    kutia.style.display='none'
+    muzik.volume=0.05
+    muzik.play()
+ setTimeout(function(){
+    intervalId = setInterval(() => {
+        requestAnimationFrame(vizato)
+    }, 10);
+ },200)
 
 }
 
 
-intervalId = setInterval(() => {
-    requestAnimationFrame(vizato)
-}, 10);
 
-let muzik = new Audio()
-muzik.src="https://www.learningcontainer.com/wp-content/uploads/2020/02/Kalimba.mp3"
 
-let button = document.querySelector("button");
+/*let button = document.querySelector("button");
 button.style.color = "red";
 button.addEventListener("click", (e) => {
   if (button.innerText == "On") {
     button.innerText = "Off";
     button.style.color = "red";
     muzik.pause();
-    audio.ended = true;
+   
   } else if (button.innerText == "Off") {
     button.innerText = "On";
     button.style.color = "green";
     muzik.play();
   }
-});
+});*/
 
+window.addEventListener('load',function(){
+    canvas.style.display = 'none'
+    start.addEventListener('click',function(){
+        fillo()
+    })
+})
